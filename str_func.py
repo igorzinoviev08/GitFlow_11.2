25c372ad59689acb9139f757c69aff87de9411eb
igorzinoviev08 <zinovievigor08@gmail.com>
2023-06-24 00:09:29 +0300

    Добавил вторую функцию и docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def str_upper(line: str):
+    """Возвращает строку заглавными буквами """
     return line.upper()
+
+
+def str_title(line: str):
+    """Возвращает строку заглавными буквами в каждом слове"""
+    return line.title()
+
+
+
